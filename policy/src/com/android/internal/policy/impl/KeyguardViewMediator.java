012033cc2ddd3f630ea770e0f39b4b51809f2951
krarvind <krarvind@gmail.com>
2012-10-30 14:12:09 +0000

    remove lockscreen
    Change-Id: Ia7cd5aa9c686701e44ae59c328806bb5b47d8250

--- policy/src/com/android/internal/policy/impl/KeyguardViewMediator.java
+++ policy/src/com/android/internal/policy/impl/KeyguardViewMediator.java
@@ -680,7 +680,7 @@ public class KeyguardViewMediator implements KeyguardViewCallback,
         }
 
         if (DEBUG) Log.d(TAG, "doKeyguard: showing the lock screen");
-        showLocked();
+        //showLocked();
     }
 
     /**
